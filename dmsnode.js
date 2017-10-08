4893f712b936d5eb6d45a4a24e6ec0b0cdaf2f54
Victor Chan <victorchan88888888@gmail.com>
2017-10-07 23:57:42 -0700

    Code to receive POST request and log to console

--- dmsnode.js
+++ dmsnode.js
@@ -4,19 +4,16 @@ http.createServer(function(req, res) {
 
     //console.dir(req.param);
 
-    var u = url.parse(req.url, true);
-    console.log(u.pathname);
-
-    //if (req.method == 'POST') {
-    //    console.log("POST");
-    //    var body = '';
-    //    req.on('data', function(data) {
-    //        body += data;
-    //        console.log("Partial body: " + body);
-    //    });
-    //    req.on('end', function() {
-    //        console.log("Body: " + body);
-    //    });
+    if (req.method == 'POST') {
+        console.log("POST");
+        var body = '';
+        req.on('data', function(data) {
+            body += data;
+            console.log("Partial body: " + body);
+        });
+        req.on('end', function() {
+            console.log("Body: " + body);
+        });
     //    res.writeHead(200, {'Content-Type': 'text/html'});
     //    res.end('post received');
     //} else {
@@ -24,7 +21,7 @@ http.createServer(function(req, res) {
     //    var html = '<html><body><form method="post" action="http://localhost:8000">Name: <input type="text" name="name" /><input type="submit" value="Submit" /></form></body>';
     //    res.writeHead(200, {'Content-Type': 'text/html'});
     //    res.end(html);
-    //}
+    }
 
 }).listen(8000, '0.0.0.0', function() {
     var host = '192.168.0.14'
