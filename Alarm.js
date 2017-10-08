1e6821fd7a5ec5c6a0f4d86e942b56a4b4531039
Christian Lee <Christian@Christian-Lees-macbook.local>
2017-10-07 19:40:19 -0700

    alarm without email sending

---
+++ Alarm.js
@@ -0,0 +1,35 @@
+class Alarm {
+  const HOURLY = 1;
+  const DAILY = 2;
+  const WEEKLY = 3;
+  constructor(push_token, alarm_id, start_time, interval, message, contact) {
+    this.push_token = push_token;
+    this.alarm_id = alarm_id;
+    this.start_time = start_time
+    this.interval = interval;
+    this.message = message;
+    this.contact = contact;
+    this.alarm_type = alarm_type;
+    this.active = true;
+    this.secondary = true;
+  }
+
+  function visit() {
+    if (this.active) {
+      this.active = false;
+      //
+    } else {
+      if (this.secondary) {
+        this.secondary = false;
+      } else {
+        this.secondary = true;
+        this.active = true;
+      }
+    }
+  }
+
+  function deactivate() {
+    this.active = false;
+  }
+
+}
